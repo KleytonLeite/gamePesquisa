cdf0dc4efa934399656657b3058fea8aeb37043b
KleytonLeite <musicpteclados@gmail.com>
2020-09-15 20:58:49 -0300

    DTO, Service

--- backend/src/main/java/com/demo/devsuperior/pesquisa/repositories/GameRepository.java
+++ backend/src/main/java/com/demo/devsuperior/pesquisa/repositories/GameRepository.java
@@ -1,9 +1,11 @@
 package com.demo.devsuperior.pesquisa.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.demo.devsuperior.pesquisa.entities.Game;
 
+@Repository
 public interface GameRepository extends JpaRepository<Game, Long> {
 
 }
