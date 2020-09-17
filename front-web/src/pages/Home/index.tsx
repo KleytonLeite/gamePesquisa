36995e16bccc59a07c3d8f5f82ef3bb102f3d3d3
KleytonLeite <musicpteclados@gmail.com>
2020-09-17 15:23:29 -0300

    Created routes of navigation

--- front-web/src/pages/Home/index.tsx
+++ front-web/src/pages/Home/index.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 import { ReactComponent as Gamer } from '../../assets/gamer.svg';
 import { ReactComponent as ArrowIcon } from '../../assets/arrow.svg';
 
@@ -13,6 +15,7 @@ const Home = () => (
       <h3 className="home-text-subtitle">
         Clique no potão abaixo e saiba quais são os jogos que os gamers estão escolhendo!
       </h3>
+      <Link to="/records">  
       <div className="home-actions">
         <button className="home-btn">
           QUERO SABER QUAIS SÃO
@@ -21,10 +24,10 @@ const Home = () => (
           <ArrowIcon />
         </div>
       </div>
+      </Link>
     </div>
-    <div className="home-image">
-      <Gamer />
-    </div>
+
+    <Gamer className="home-image" />
 
   </div>
 );
---
+++ front-web/src/pages/Records/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './styles.css';
+
+const Records = () => (
+  <div className="home-container">
+    <h1>RECORDS</h1>
+  </div>
+);
+
+export default Records;
